185f9f1ee1a83356d74575dd377f0cb905c0bfc7
P S Saravanan <saravanp@thoughtworks.com>
2013-04-13 16:23:53 +0530

    construct url_hash

--- crawler.rb
+++ crawler.rb
@@ -6,16 +6,20 @@ ERRORs = []
 
 source_location = "http://localhost:3000"
 
-ARGV.each do |arg_url|
-  URLs << {url: arg_url, params: nil, base_url: arg_url}
+def construct_hash url
+  base_url, params = url.split("?")[0], url.split("?")[1]
+  params_key = params.split("&").map {|p| p[0...p.index('=')]} if params
+  url_hash = {base_url: base_url, params: params_key, url:url}
+  url_hash
 end
 
-def check_for_exists url_to_check
-  return unless /^\/.*/.match(url_to_check)
+ARGV.each do |arg_url|
+  URLs << construct_hash(arg_url)
+end
 
-  url, params = url_to_check.split("?")[0], url_to_check.split("?")[1]
-  params_key = params.split("&").map {|p| p[0...p.index('=')]} if params
-  url_hash = {base_url: url, params: params_key, url:url_to_check}
+def check_for_exists url
+  return unless /^\/.*/.match(url)
+  url_hash = construct_hash url
   already_exist = URLs.detect do |parsed_url|
     parsed_url[:base_url] == url_hash[:base_url] && parsed_url[:params] == url_hash[:params]
   end
